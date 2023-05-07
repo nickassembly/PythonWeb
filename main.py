4f5a8d42bf37d7cb1a095aa30aaaebf559a662d4
nickguerra <nickg@maliciousgames.com>
2023-05-07 06:39:23 -0500

    added basic browser request

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-# placeholder
+import requests
 
-print(f"hello keller")
+response = requests.get('https://en.wikipedia.org/wiki/Khabib_Nurmagomedov')
+print(response.status_code)
+print(response.text)
+
+with open('khabib_2.html', 'w', encoding='utf-8') as f:
+    f.write(response.text)
+
+# with open('khabib.html', 'r', encoding='utf-8') as f:
+#     contents = f.read()
+#     print(contents)
